d983ef0d19b564ba8a295243b44b53cd276f7109
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2021-05-31 16:50:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const endPoint = "http://localhost:3000/api/v1/neighborhoods" 
 
 document.addEventListener('DOMContentLoaded', () => {
+    getCocktails()
+
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(cocktails => {
+      cocktails.data.forEach(cocktail => {
+        
+        render(cocktail)
+      })
+    })
+}
