732e25953c0c808c47080a503cfc8e4158fc6923
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2021-06-06 01:12:38 -0400

    updated index

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const endPoint = "http://localhost:3000/api/v1/neighborhoods"
 document.addEventListener('DOMContentLoaded', () => {
     getCocktails()
 
+
+    const createCocktailForm = document.querySelector ("#create-cocktail-form")
+
     fetch(endPoint)
     .then(response => response.json())
     .then(cocktails => {
