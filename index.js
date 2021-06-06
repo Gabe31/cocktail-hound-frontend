10acc58219e8f5bb9c6d38ef45ac93d4bc95eb65
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2021-06-06 15:42:04 -0400

    updated html page

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const createCocktailForm = document.querySelector ("#create-cocktail-form")
 
+    createCocktailForm.addEventListener("submit", (e) => createFormHandler(e))
+})
+
     fetch(endPoint)
     .then(response => response.json())
     .then(cocktails => {
@@ -15,3 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
       })
     })
 }
+
+function render(cocktail) {
+    const cocktailMarkup = `
+      <div data-id=${cocktail.id}>
+        <h3>${cocktail.attributes.title}</h3>
+        <p><strong>Description:</strong> ${cocktail.attributes.description}</p>
+        <p><strong>Bar:</strong> ${cocktail.attributes.bar}</p>
+        
+        <p><strong>Neighborhood:</strong> ${cocktail.attributes.neighborhood.name}</p>
+        <button data-id=${cocktail.id}>edit</button>
+      </div>
+      <br><br>`;
+  
+    document.querySelector('#cocktail-container').innerHTML += cocktailMarkup
+  }
+  
+  
+  function createFormHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const barInput = document.querySelector('#input-bar').value
+    const neighborhoodId = parseInt(document.querySelector('#neighborhoods').value)
+    postAdventures (titleInput, descriptionInput, barInput, neighborhoodId)
+  }
