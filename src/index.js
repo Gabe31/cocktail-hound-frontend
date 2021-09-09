3c41ed3c07eb70b8f4659ae892af8b85626bf6e5
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2021-09-09 13:38:51 -0400

    sept 9

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const endPoint = "http://localhost:3000/api/v1/cocktails" 
+const endPoint = "http://localhost:3000/api/v1/neighborhoods" 
 
 document.addEventListener('DOMContentLoaded', () => {
     getCocktails()
@@ -9,53 +9,57 @@ document.addEventListener('DOMContentLoaded', () => {
     createCocktailForm.addEventListener("submit", (e) => createFormHandler(e))
 })
 
-document.addEventListener("click", function(e) {
-  const cocktailCard = document.getElementById(`${e.target.dataset.id}`)
-  if(e.target.matches("#delete-btn")) {
-    e.preventDefault()
-    deleteCocktail(e.target.dataset.id)
-    cocktailCard.remove(cocktailCard)
-  }
-})
-
-
- function getCocktails() {
-    fetch (endPoint)
-    .then (response => response.json())
-    .then (cocktails => {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(cocktails => {
       cocktails.data.forEach(cocktail => {
-        let newCocktail = new Cocktail(cocktail, cocktail.attributes)
-        document.querySelector("#cocktail-container").innerHTML += newCocktail.renderCocktailCard()
+        
+        render(cocktail)
       })
     })
-  }
+}
 
+function render(cocktail) {
+    const cocktailMarkup = `
+      <div data-id=${cocktail.id}>
+        <h3>${cocktail.attributes.title}</h3>
+        <p><strong>Description:</strong> ${cocktail.attributes.description}</p>
+        <p><strong>Bar:</strong> ${cocktail.attributes.bar}</p>
+        
+        <p><strong>Neighborhood:</strong> ${cocktail.attributes.neighborhood.name}</p>
+        <button data-id=${cocktail.id}>edit</button>
+      </div>
+      <br><br>`;
+  
+    document.querySelector('#cocktail-container').innerHTML += cocktailMarkup
+  }
+  
+  
   function createFormHandler(e) {
     e.preventDefault()
     const titleInput = document.querySelector('#input-title').value
     const descriptionInput = document.querySelector('#input-description').value
     const barInput = document.querySelector('#input-bar').value
     const neighborhoodId = parseInt(document.querySelector('#neighborhoods').value)
-    postCocktails (titleInput, descriptionInput, barInput, neighborhoodId)
+    postAdventures (titleInput, descriptionInput, barInput, neighborhoodId)
   }
 
-function postCocktails(title, description, bar, neighborhood_id) {
-  let bodyObj = {title, description, bar, neighborhood_id}
-
-  fetch(endPoint, {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(bodyObj)
-  })
-  .then(response => response.json())
-  .then(cocktail => {
-    const cocktailData =cocktail.data
-
-    let newCocktail = new Cocktail(cocktailData, cocktailData.attributes)
-
-    document.querySelector('#cocktail-container').innerHTML += newCocktail.renderCocktailCard()
+  function postCocktails(title, description, bar, neighborhood_id) {
+    let bodyObj = {title, description, bar, category_id}
+    
+    fetch(endPoint, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(bodyObj)
     })
+    .then(response => response.json())
+    .then(cocktail => {
+      const cocktailData = cocktail.data
+      
+      let newCocktail = new Cocktail(cocktailData, cocktailData.attributes)
 
+      document.querySelector('#cocktail-container').innerHTML += newCocktail.renderCocktailCard()
+})
 }
 
 
@@ -65,4 +69,6 @@ function deleteCocktail(id) {
     method: "DELETE" 
 })
   .then(response => response.json())
+  .then(response => console.log(response))
+
 }
